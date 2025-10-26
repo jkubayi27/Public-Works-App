f1bb14e1acd191d8d0a46184367b8d1030ff30c5
Julio_De_Pro <jkubayi27@gmail.com>
2025-10-26 21:51:58 +0200

    Established session without using Passport

--- my-backend/index.js
+++ my-backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const {Pool} = require("pg");
 const cors = require("cors");
+const passport = require("passport");
 require("dotenv").config();
+const session = require("express-session");
+const Strategy = require("passport-local");
+const e = require("express");
 
 const app = express();
 app.use(cors());
@@ -16,22 +20,36 @@ const pool = new Pool({
     port : process.env.DB_PORT,
 });
 
-//Check username & password
-//Install bcrypt module for hashing and protecting user password
+//Create session
+app.use(session({
+    secret : "JulioSecret",
+    resave : false,
+    saveUninitialized : false,
+    cookie : {secure : false}
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Login using session
 app.get('/authenticate', async (req,res) => {
     const {username,password} = req.query;
     try {
-        const result = await pool.query('SELECT username,password FROM users WHERE username = $1 AND password = $2',[username,password]);
-        correctPassword = result.rows[0].password;
-        if (correctPassword == password){
+        const result = await pool.query('SELECT * FROM users WHERE username = $1',[username]);
+        const user = result.rows[0];
+        console.log(user);
+        if (!user) {
+            console.log('User not found')
+        } else if (user.password == password) {
+            //Store user credentials in session
+            req.session.user = {id : user.id, username : user.username};
             res.json(true);
-            console.log('Login successful')
         } else {
-            res.send(false)
-            console.log('Incorrect password or username');
-        }  
+            res.json(false);
+            console.log('Invalid credentials');
+        }
     } catch(err) {
-        console.log('Login query error',err);
+        console.log(err);
     }
 })
 //Example route
@@ -126,7 +144,39 @@ app.get("/report",async (req,res) => {
         console.log(err);
         console.log("Failed to get trade occurences");
     }
-})
+});
+
+//Authentication strategy
+passport.use(new Strategy (async function verify(username,password, cb) {
+    console.log(username);
+    try {
+        const result = await pool.query('SELECT * FROM users WHERE username = $1 AND password = $2',[username,password]);
+        if (result.rows.length > 0) {
+            const user = result.rows[0];
+            const correctPassword = user.password;
+            if (correctPassword == password){
+                console.log('Login successful');
+                return cb(null,user)
+            } else {
+                console.log('Incorrect password or username');
+                return cb(null,false);
+            }
+        }  else {
+            return cb('User not found');
+        }
+    } catch(err) {
+        return cb(err);
+    }
+}));
+
+//serialize & deserialize passport
+passport.serializeUser((user,cb) => {
+    cb(null,user);
+});
+
+passport.deserializeUser((user,cb) => {
+    cb(null,user);
+});
 
 //Start server
 const PORT = process.env.PORT || 5000;
---
+++ public-works/src/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav () {
+    return (
+        <nav className="navbar">
+            <ul className="nav-links">
+                <li>Create Works Order</li>
+                <li>Filter & Search</li>
+                <li>View Report</li>
+            </ul>
+        </nav>
+    )
+}
